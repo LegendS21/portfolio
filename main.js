4a76bafb138f7049368180b5c8a3517ff05382ca
LegendS21 <melvinsumardi21@gmail.com>
2023-09-22 23:26:54 +0800

    add about

---
+++ main.js
@@ -0,0 +1,12 @@
+function bukatutuptogle(e){
+    const target = $(e.target)
+    if($(target).hasClass("active")){
+        $(target).html("more info").removeClass("active")
+    }
+    else {
+        $(target).html("less info").addClass("active")
+    }
+    const item = $(target).parents(".about-exp-item")
+    const detail = $(item).children(".about-exp-item-detail")
+    $(detail).slideToggle()
+}
