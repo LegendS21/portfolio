d4de9934945b30310885002155bb2728f4528011
LegendS21 <melvinsumardi21@gmail.com>
2023-09-24 11:51:54 +0800

    add contact

--- main.js
+++ main.js
@@ -10,3 +10,25 @@ function bukatutuptogle(e){
     const detail = $(item).children(".about-exp-item-detail")
     $(detail).slideToggle()
 }
+function onformsubmit(e){
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const massage = $("#inp_massage")
+
+    if(!$(email).val()){
+        alert("email is required")
+    }
+    else if (!$(subject).val()){
+        alert("subject is required")
+    }
+    else if(!$(massage).val()){
+        alert("massage is required")
+    }
+    else{
+        alert("form submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(massage).val("")
+    }
+}
